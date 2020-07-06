3f5513cd5240f9a084caf05c95ddf19c9c73de6a
Scott White <spwhite1337@gmail.com>
2020-07-05 22:55:51 -0400

    Wrap predictions for cli

--- card_classifier/predictions.py
+++ card_classifier/predictions.py
@@ -3,7 +3,7 @@ import argparse
 from modeling.classifier import MagicCardClassifier
 
 
-def predict():
+def predict_cli():
     parser = argparse.ArgumentParser(prog='Predict from Magic Card Classifier')
     parser.add_argument('--input_path', type=str, required=True)
     parser.add_argument('--version', type=str, default='v1')
@@ -11,11 +11,15 @@ def predict():
     parser.add_argument('--save_path', type=str, required=False)
     args = parser.parse_args()
 
+    predict(version=args.version, model_type=args.model_type, input_path=args.input_path, save_path=args.save_path)
+
+
+def predict(version: str, model_type: str, input_path: str, save_path: str = None):
     # Instantiate classifier and load models
-    mcc = MagicCardClassifier(version=args.version, model_type=args.model_type, load=True)
+    mcc = MagicCardClassifier(version=version, model_type=model_type, load=True)
 
     # Predict
-    outputs = mcc.predict(args.input_path)
-    if args.save_path:
-        with open(args.save_path, 'wb') as fp:
+    outputs = mcc.predict(input_path)
+    if save_path:
+        with open(save_path, 'wb') as fp:
             pickle.dump(outputs, fp)
