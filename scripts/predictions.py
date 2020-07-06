333316bfec4b9c66031fe07519547b0afb784e06
Scott White <spwhite1337@gmail.com>
2020-07-05 22:07:36 -0400

    save outputs

--- scripts/predictions.py
+++ scripts/predictions.py
@@ -1,3 +1,4 @@
+import pickle
 import argparse
 from modeling.classifier import MagicCardClassifier
 
@@ -7,10 +8,14 @@ def predict():
     parser.add_argument('--input_path', type=str, required=True)
     parser.add_argument('--version', type=str, default='v1')
     parser.add_argument('--model_type', type=str, default='VGG')
+    parser.add_argument('--save_path', type=str, required=False)
     args = parser.parse_args()
 
     # Instantiate classifier and load models
     mcc = MagicCardClassifier(version=args.version, model_type=args.model_type, load=True)
 
     # Predict
-    mcc.predict(args.input_path)
+    outputs = mcc.predict(args.input_path)
+    if args.save_path:
+        with open(args.save_path, 'wb') as fp:
+            pickle.dump(outputs, fp)
