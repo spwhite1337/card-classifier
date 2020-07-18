180e9bb0dbe75635b5df78eb033d04bfc442e8db
Scott White <spwhite1337@gmail.com>
2020-07-18 07:03:15 -0400

    Args to skip results / data in upload

--- card_classifier/upload.py
+++ card_classifier/upload.py
@@ -1,9 +1,17 @@
 import os
+import argparse
 from config import Config, logger
 
 
 def upload():
-    logger.info('Uploading Data')
-    os.system('aws s3 sync {} {} --exclude .gitignore'.format(Config.DATA_DIR, Config.CLOUD_DATA))
-    logger.info('Uploading Results')
-    os.system('aws s3 sync {} {} --exclude .gitignore'.format(Config.RESULTS_DIR, Config.CLOUD_RESULTS))
+    parser = argparse.ArgumentParser(prog='Upload card classifier')
+    parser.add_argument('--skipdata', action='store_true')
+    parser.add_argument('--skipresults', action='store_true')
+    args = parser.parse_args()
+
+    if not args.skipdata:
+        logger.info('Uploading Data')
+        os.system('aws s3 sync {} {} --exclude .gitignore'.format(Config.DATA_DIR, Config.CLOUD_DATA))
+    if not args.skipresults:
+        logger.info('Uploading Results')
+        os.system('aws s3 sync {} {} --exclude .gitignore'.format(Config.RESULTS_DIR, Config.CLOUD_RESULTS))
