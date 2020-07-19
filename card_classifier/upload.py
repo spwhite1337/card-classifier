ec7e2f81059e6d6b52e67a84cb4a9cc895cc2f1a
Scott White <spwhite1337@gmail.com>
2020-07-19 14:26:43 -0400

    Update upload command

--- card_classifier/upload.py
+++ card_classifier/upload.py
@@ -1,17 +1,36 @@
 import os
+import re
 import argparse
 from config import Config, logger
 
 
 def upload():
     parser = argparse.ArgumentParser(prog='Upload card classifier')
+    parser.add_argument('--windows', action='store_true')
     parser.add_argument('--skipdata', action='store_true')
     parser.add_argument('--skipresults', action='store_true')
+    parser.add_argument('--dryrun', action='store_true')
     args = parser.parse_args()
+
+    # General commands
+    sync_base = 'aws s3 sync '
+    dryrun_arg = ' --dryrun'
+    results_sync = '{} {}'.format(Config.CLOUD_RESULTS, Config.RESULTS_DIR)
+    data_sync = '{} {}'.format(Config.CLOUD_DATA, Config.DATA_DIR)
+
+    include_flag = " -- exclude '.gitignore'"
+    if args.windows:
+        include_flag = re.sub("'", "", include_flag)
 
     if not args.skipdata:
         logger.info('Uploading Data')
-        os.system("aws s3 sync {} {} --exclude '.gitignore'".format(Config.DATA_DIR, Config.CLOUD_DATA))
+        cc_sync = sync_base + data_sync + include_flag
+        cc_sync += dryrun_arg if args.dryrun else ''
+        logger.info(cc_sync)
+        os.system(cc_sync)
     if not args.skipresults:
         logger.info('Uploading Results')
-        os.system("aws s3 sync {} {} --exclude '.gitignore'".format(Config.RESULTS_DIR, Config.CLOUD_RESULTS))
+        cc_sync = sync_base + results_sync + include_flag
+        cc_sync += dryrun_arg if args.dryun else ''
+        logger.info(cc_sync)
+        os.system(cc_sync)
