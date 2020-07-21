33205d80665f4ff2933e1ed49fa781ba1af55603
Scott White <spwhite1337@gmail.com>
2020-07-21 12:20:25 -0400

    typo in argparse

--- card_classifier/experiments.py
+++ card_classifier/experiments.py
@@ -12,7 +12,7 @@ def run_experiments():
     parser.add_argument('--debug', action='store_true')
     parser.add_argument('--version', type=str, default='v0')
     parser.add_argument('--overwrite', action='store_true')
-    parser.add_argument('--color', typ=str, required=False)
+    parser.add_argument('--color', type=str, required=False)
     args = parser.parse_args()
 
     # Load experiments
